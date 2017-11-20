0eec29431d0aa492e09680aedef847bcadad0a11
c5vivian <vvlyu@brandeis.edu>
2017-11-20 10:36:26 -0500

    print Chinese

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
   public static void main(String[] args){
       System.out.println("Hello World!");
+  public static void printChinese(){
+      System.out.println("山");
+    }
+  
   }
 }
